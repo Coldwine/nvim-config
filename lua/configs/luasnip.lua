104a3e41e9c6bec46f118045df05058fd163885f
Marcel Krause <m.krause@hellobetter.de>
2024-12-03 12:17:23 +0100

    Combine 4 keymaps into 2

--- lua/configs/luasnip.lua
+++ lua/configs/luasnip.lua
@@ -1,7 +1,5 @@
-require("luasnip.loaders.from_lua").lazy_load()
+require('luasnip.loaders.from_lua').lazy_load()
 
 -- set keybinds for both INSERT and VISUAL.
-vim.api.nvim_set_keymap("i", "<C-n>", "<Plug>luasnip-next-choice", {})
-vim.api.nvim_set_keymap("s", "<C-n>", "<Plug>luasnip-next-choice", {})
-vim.api.nvim_set_keymap("i", "<C-p>", "<Plug>luasnip-prev-choice", {})
-vim.api.nvim_set_keymap("s", "<C-p>", "<Plug>luasnip-prev-choice", {})
+vim.api.nvim_set_keymap({ 'i', 's' }, '<C-n>', '<Plug>luasnip-next-choice', {})
+vim.api.nvim_set_keymap({ 'i', 's' }, '<C-p>', '<Plug>luasnip-prev-choice', {})
